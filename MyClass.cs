21869d1dee68bd1fa52c281c442289d83b1d14cf
megazyz <megasys@intro.hu>
2025-04-21 17:34:42 +0200

    Initial commit

---
+++ MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace ReactiveUI.SourceGenerators.Repro1;
+
+public class MyReactiveBase : IReactiveObject
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public event PropertyChangingEventHandler? PropertyChanging;
+
+    public void RaisePropertyChanging(PropertyChangingEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RaisePropertyChanged(PropertyChangedEventArgs args)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class MyClass : MyReactiveBase
+{
+    [Reactive] public partial string TestProp { get; set; }
+}
